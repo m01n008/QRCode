60f78a51642866b4f456648cd0d178d9b42f8dd5
m01n008 <iamoin008@gmail.com>
2024-10-03 15:11:48 +0500

    - QR Generator feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation (libs.androidx.camera.lifecycle)
     implementation (libs.androidx.camera.view)
     implementation (libs.barcode.scanning)
+    implementation(libs.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/moin/qrcode/ui/screens/QRGeneration/QRGeneratorView.kt
+++ app/src/main/java/com/moin/qrcode/ui/screens/QRGeneration/QRGeneratorView.kt
@@ -1,10 +1,15 @@
 package com.moin.qrcode.ui.screens.QRGeneration
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.get
 import androidx.fragment.app.Fragment
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.qrcode.QRCodeWriter
 import com.moin.qrcode.databinding.FragmentQrgeneratorBinding
 
 class QRGeneratorView: Fragment() {
@@ -16,13 +21,34 @@ class QRGeneratorView: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _fragmentQRGenBinding = FragmentQrgeneratorBinding.inflate(inflater,container, false)
+        fragmentQRGenBinding.genQR.setOnClickListener {
+            generateQRCode()
+        }
         return fragmentQRGenBinding.root
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _fragmentQRGenBinding = null
     }
+
+fun  generateQRCode(){
+    val content = fragmentQRGenBinding.qrInputText.text.toString()
+    val qrCodeWriter = QRCodeWriter()
+    val bitmapMatrix = qrCodeWriter.encode(content,BarcodeFormat.QR_CODE,512,512)
+    val bitmapWidth = bitmapMatrix.width
+    val bitmapHeight = bitmapMatrix.height
+    val bitmap = Bitmap.createBitmap(bitmapWidth,bitmapHeight,Bitmap.Config.RGB_565)
+    for (x in 0 until bitmapWidth){
+        for (y in 0 until bitmapHeight){
+            bitmap.setPixel(x, y, if (bitmapMatrix.get(x,y)) Color.BLACK else Color.WHITE)
+        }
+    }
+    fragmentQRGenBinding.qrImageView.setImageBitmap(bitmap)
+
+}
 
 
 }
